fba79002ebbfb7e7e73bfd33ac572ce2d409285f
jhordancamacho <jhordan.camacho@gmail.com>
2022-02-28 20:49:43 -0500

    update build

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
-import Header from './components/Header/Header.js';
 import { Route } from 'wouter';
+import { Helmet } from 'react-helmet';
+import Header from './components/Header/Header.js';
 import Slider from './components/Slider/Slider';
 import Bienvenida from './components/Bienvenida/Bienvenida';
 import ProductosPrincipales from './components/ProductosPrincipales/ProductosPrincipales';
@@ -10,6 +11,12 @@ import BenditaEmpresa from './components/BenditaEmpresa/BenditaEmpresa';
 
 function App() {
   return (<>
+  <Helmet>
+  <meta charset="utf-8" />
+  <title>Bendita Empanada</title>
+  <link rel="canonical" href="http://github.com/jhordancamacho"/>
+  <meta name="description" content="Testin"/>
+</Helmet>
     <Header ></Header>
     <Slider></Slider>
       <Route path="/" component={Bienvenida} />
@@ -23,6 +30,13 @@ function App() {
     {/*Ruta Bendita empanada ''productos*/}
     <Route path="/empanada" component={ProductosPrincipales} />
     
+    {/* ruta para contacto */}
+    <Route path="/contacto" component={ContactoHome} />
+
+    {/* para cualquier ruta no definida */}
+      <Route path=".." component={Bienvenida} />
+      <Route path=".." component={ProductosPrincipales} />
+    <Route path=".." component={ContactoHome} />
 
     <InformacionContacto></InformacionContacto>
   </>
